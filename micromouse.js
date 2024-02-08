7927fd47652b9b5855efd867753c568f4fc4d454
IronTaurus <irontauru@gmail.com>
2024-02-08 16:15:43 +0100

    ProjectDisplay

---
+++ micromouse.js
@@ -0,0 +1,7 @@
+console.log("Micro Mouse!")
+const projectDisplay = document.getElementById("ProjectDisplay");
+var title = document.createElement("h1");
+var textNode = document.createTextNode("Micro mouse is displayed!");
+title.appendChild(textNode);
+projectDisplay.appendChild(title);
+
