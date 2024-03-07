73d1d7da0812d69940b7e9cb80ed76a00bc6345c
IronTaurus <irontauru@gmail.com>
2024-03-07 12:20:33 +0100

    precon maze constructor

--- pathfinder.js
+++ pathfinder.js
@@ -1,3 +1,67 @@
+console.log("Micro Mouse!")
+var display = document.getElementById("ProjectDisplay")
+const canvasElement = document.createElement("canvas");
+canvasElement.setAttribute("class", "maze");
+display.appendChild(canvasElement);
+let maze = document.querySelector(".maze");
+const ctx = maze.getContext("2d");
+
+let current;
+
+const tlCorner={
+    topWall: true,
+    rightWall: false,
+    botWall: false,
+    leftWall: true
+}
+const trCorner = {
+    topWall: true,
+    rightWall: true,
+    botWall: false,
+    leftWall: false
+}
+const blCorner = {
+    topWall: false,
+    rightWall: false,
+    botWall: true,
+    leftWall: true
+}
+const brCorner = {
+    topWall: false,
+    rightWall: true,
+    botWall: true,
+    leftWall: false
+}
+const xHall = {
+    topWall: true,
+    rightWall: false,
+    botWall: true,
+    leftWall: false
+}
+const yHall = {
+    topWall: false,
+    rightWall: true,
+    botWall: false,
+    leftWall: true
+}
+const blOuterCorner = {
+    topWall: true,
+    rightWall: false,
+    botWall: true,
+    leftWall: true
+}
+const end = {
+    topWall: true,
+    rightWall: true,
+    botWall: true,
+    leftWall: false
+}
+const row0 = [tlCorner, xHall, xHall, xHall, trCorner]
+const row1 = [yHall, tlCorner, xHall, trCorner, yHall]
+const row2 = [yHall, blCorner,end, yHall, yHall]
+const row3 = [blCorner, xHall, xHall, brCorner, yHall]
+const row4 = [blOuterCorner, xHall, xHall, xHall, brCorner]
+const preconRows = [row0,row1,row2,row3,row4];
 
 class Maze {
     constructor(size, cols, rows){
@@ -14,17 +78,34 @@ class Maze {
             let row = [];
             for (let c = 0; c < this.cols; c++) {
                 let cell = new Cell(r, c, this.grid, this.size)
+                cell.walls = preconRows[r][c];
                 row.push(cell);
-            }
+            }   
             this.grid.push(row)
         }
-        current = this.grid[0][0]; //starts the maze at cell 0,0 (topleft)
+        current = this.grid[4][2]; //starts the maze at coords
+        let goal = this.grid[2][2]; //goal coord
+        goal.goal = true;
+    }
+    Draw() {
+        console.log("Entering draw function...")
+        maze.width = this.size;
+        maze.height = this.size;
+        maze.style.background = "black";
+
+        current.visited = true;
+        for (let r = 0; r < this.rows; r++) {
+            for (let c = 0; c < this.cols; c++) {
+                let grid = this.grid;
+                grid[r][c].show(this.size, this.rows, this.cols);
+            }
+        }
     }
 }
 
 class Cell {
     constructor(rowNum, colNum, parentGrid, parentSize){
-        this.goal = 0;
+        this.goal = false;
         this.rowNum = rowNum;
         this.colNum = colNum;
         this.parentGrid = parentGrid;
@@ -36,9 +117,58 @@ class Cell {
             leftWall: true
         }
     }
+    drawTopWall(x, y, size, cols, rows) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        ctx.lineTo(x + size / cols, y);
+        ctx.strokeStyle = "green";
+        ctx.stroke();
+    };
+    drawRightWall(x, y, size, cols, rows) {
+        ctx.beginPath();
+        ctx.moveTo(x + size / cols, y);
+        ctx.lineTo(x + size / cols, y + size / rows);
+        ctx.strokeStyle = "black";
+        ctx.stroke();
+    }
+    drawBotWall(x, y, size, cols, rows) {
+        ctx.beginPath();
+        ctx.moveTo(x, y + size / rows);
+        ctx.lineTo(x + size / cols, y + size / rows);
+        ctx.strokeStyle = "blue";
+        ctx.stroke();
+    }
+    drawLeftWall(x, y, size, cols, rows) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        ctx.lineTo(x, y + size / rows);
+        ctx.strokeStyle = "red";
+        ctx.stroke();
+    }
+
+    show(size, rows, cols) {
+        console.log("Entering show function...")
+        let x = (this.colNum * size) / cols;
+        let y = (this.rowNum * size) / rows;
+
+        ctx.strokeStyle = "white";
+        ctx.fillStyle = "blue";
+        ctx.lineWidth = "2";
+
+        console.log(`${this.rowNum}: T:${this.walls.topWall}, R:${this.walls.rightWall}, B:${this.walls.botWall}, L:${this.walls.leftWall}`)
+        if (this.walls.topWall) this.drawTopWall(x, y, size, cols, rows);
+        if (this.walls.rightWall) this.drawRightWall(x, y, size, cols, rows);
+        if (this.walls.botWall) this.drawBotWall(x, y, size, cols, rows);
+        if (this.walls.leftWall) this.drawLeftWall(x, y, size, cols, rows);
+        if (this.visited) {
+            ctx.fillRect(x + 1, y + 1, size / cols - 2, size / rows - 2)
+        }
+        if (this.goal){
+            ctx.fillStyle = "yellow";
+            ctx.fillRect(x + 1, y + 1, size / cols - 2, size / rows - 2)
+        }
+    }
 }
-
-
 
 class Mouse {
     constructor(currentCell){
@@ -48,41 +178,9 @@ class Mouse {
     }
 }
 
-function CreateMaze(){
 
-}
-
-function checkWall(t, r, b, l)
-{
-    if(currentCell.topWall == t){
-        const myCell = currentCell;
-        myCell.rowNum--;
-        currentCell = myCell;
-    }
-    else if(currentCell.rightWall == r){
-        const myCell = currentCell;
-        myCell.rowNum--;
-        currentCell = myCell;
-    }
-    else if(currentCell.bottomWall == b){
-        const myCell = currentCell;
-        myCell.rowNum--;
-        currentCell = myCell;
-    }
-    else if(currentCell.topWall == l){
-        const myCell = currentCell;
-        myCell.rowNum--;
-        currentCell = myCell;
-    }
-}
-
-function PathFinder(){
-let myMouse = new Mouse();
-// checkWall();
-
-    
-}
-
-
+const myMaze = new Maze(500, 5, 5);
+myMaze.Setup();
+myMaze.Draw();
 
 
